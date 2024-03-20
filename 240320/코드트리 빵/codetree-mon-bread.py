2db164be18d47439f31146dd009fc5eccf2e0f11
Kim Hyeonah <48481035+tribbless@users.noreply.github.com>
2024-03-20 15:54:43 +0900

    Title: 코드트리 빵, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240320/코드트리 빵/codetree-mon-bread.py
@@ -0,0 +1,23 @@
+n,m = map(int,input().split())
+
+arr = []
+for i in range(n):
+    arr.append(list(map(int,input().split(' '))))
+
+conv=[]
+for j in range(m):
+    x,y = map(int,input().split())
+    conv.append((x,y))
+    arr[x-1][y-1] = 2
+
+base = []
+for i in range(5):
+    for j in range(5):
+        if arr[i][j] == 1:
+            base.append((i+1,j+1))
+            break
+res = []
+for i in range(m):
+    res.append(i +1 + abs(conv[i][0]-base[i][0]) + abs(conv[i][1]-base[i][1]))
+
+print(max(res))
